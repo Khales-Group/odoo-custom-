f784048bd6891aa6379b0c0cef424fb7b3334273
Khales Group <it@khales.ae>
2025-10-21 14:43:47 +0400

    dept: add model (name, company_id, active) + access

--- addons/kh_approvals/models/department.py
+++ addons/kh_approvals/models/department.py
@@ -1,10 +1,16 @@
-from odoo import fields, models
+# -*- coding: utf-8 -*-
+from odoo import api, fields, models
 
 class KhApprovalsDepartment(models.Model):
     _name = "kh.approvals.department"
     _description = "Approvals Department"
     _rec_name = "name"
-    _check_company_auto = True
 
     name = fields.Char(required=True)
-    company_id = fields.Many2one("res.company", default=lambda self: self.env.company, required=True, index=True)
+    company_id = fields.Many2one(
+        "res.company",
+        string="Company",
+        default=lambda self: self.env.company,
+        index=True,
+    )
+    active = fields.Boolean(default=True)
