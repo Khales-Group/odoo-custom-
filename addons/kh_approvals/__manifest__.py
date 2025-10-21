4c55d0cd8d00ea120f96d3366d9aef97c6c82986
Khales Group <it@khales.ae>
2025-10-21 12:14:35 +0400

    Approvals: company + department + views + sequences + MC rules

--- addons/kh_approvals/__manifest__.py
+++ addons/kh_approvals/__manifest__.py
@@ -6,21 +6,19 @@
     "author": "Khales Team",
     "website": "https://khales.ae",
     "category": "Operations/Approvals",
-    "depends": ["base", "mail"],   # no need to add 'discuss' because we guard its usage
-    "data": [      
-        "security/kh_approvals_mc_rules.xml",
-        "data/sequence.xml",
-        "views/department_views.xml",
-        # security first
-        "security/ir.model.access.csv",
-        "security/kh_approvals_security.xml",
-        "security/kh_approvals_rules.xml",
-        # "views/mail_activity_views.xml",
-        "views/approval_rule_views.xml",
-        "views/approval_request_views.xml",
-        "views/menu.xml",
-    ],
-    "application": True,
+ "depends": ["base", "mail"],
+"data": [
+    "security/ir.model.access.csv",
+    "security/kh_approvals_security.xml",
+    "security/kh_approvals_rules.xml",
+    "security/kh_approvals_mc_rules.xml",  # ← multi-company rules
+    "data/sequence.xml",                    # ← company-scoped sequence
+    "views/department_views.xml",           # ← optional dept master
+    "views/approval_rule_views.xml",
+    "views/approval_request_views.xml",
+    "views/menu.xml",
+],
+   "application": True,
     "installable": True,
     "license": "LGPL-3",
 }
