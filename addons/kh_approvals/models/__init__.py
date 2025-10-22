7e4ec64c31418161f52665a9a2bb4ca580df689d
Khales Group <it@khales.ae>
2025-10-22 14:36:12 +0400

    approvals: silence emails on state changes; keep full file

--- addons/kh_approvals/models/__init__.py
+++ addons/kh_approvals/models/__init__.py
@@ -2,3 +2,4 @@ from . import approval_request
 from . import department
 from . import mail_activity
 from . import rule_step   # <-- add this line
+from . import mail_activity_guard   # <-- add this line
