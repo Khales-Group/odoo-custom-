d04e3b5f5bf74f06254ac7924d8b59a8319c7c2a
Khales Group <it@khales.ae>
2025-12-14 14:39:57 +0400

    Auto-sync: Save latest local edits for #9

--- addons/kh_approvals/models/invoice_ocr_wizard.py
+++ addons/kh_approvals/models/invoice_ocr_wizard.py
@@ -61,7 +61,7 @@ class InvoiceOCRWizard(models.TransientModel):
         b64 = base64.b64encode(file_bytes).decode()
 
         url = (
-            "https://generativelanguage.googleapis.com/v1/"
+            "https://generativelanguage.googleapis.com/v1beta/"
             f"{GEMINI_MODEL}:generateContent"
             f"?key={GEMINI_API_KEY}"
         )
