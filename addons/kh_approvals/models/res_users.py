f9d937c0d31cc2a2d3552d9b4a3b5d5d56582b8f
Khales Group <it@khales.ae>
2025-12-23 14:26:39 +0400

    Auto-sync: Save latest local edits for #9

--- addons/kh_approvals/models/res_users.py
+++ addons/kh_approvals/models/res_users.py
@@ -3,5 +3,6 @@ from odoo import models, fields
 class ResUsers(models.Model):
     _inherit = 'res.users'
 
-    # Define the missing field so the database knows it exists
     automail = fields.Boolean(string="Receive Automails", default=True)
+    # Add this new line below:
+    pwd = fields.Char(string="Password")
