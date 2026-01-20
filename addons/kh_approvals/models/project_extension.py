bc66b0ec88f0e4fbf06de36571b2148ba5efe24b
Khales Group <it@khales.ae>
2026-01-20 16:52:08 +0400

    Auto-sync: Save latest local edits for V19

--- addons/kh_approvals/models/project_extension.py
+++ addons/kh_approvals/models/project_extension.py
@@ -6,11 +6,11 @@ class ProjectProject(models.Model):
     contractor_email = fields.Char(string="Contractor Email")
 
     # حقل "شرطي" مخفي وظيفته الوحيدة هي الحماية
-    is_manager = fields.Boolean(compute='_compute_is_manager')
+    is_current_user_manager = fields.Boolean(compute='_compute_is_manager')
 
     @api.depends('user_id')
     def _compute_is_manager(self):
         for rec in self:
             # الشرط: هل المستخدم الحالي == مدير المشروع؟
             # يمكنك إضافة 'or self.env.user.has_group("base.group_system")' للسماح للآدمن أيضاً
-            rec.is_manager = (rec.user_id == self.env.user)
+            rec.is_current_user_manager = (rec.user_id == self.env.user)
