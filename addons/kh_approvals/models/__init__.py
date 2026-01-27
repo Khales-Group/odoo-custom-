1e061b46abd1214dd636dbe0453e8009124684a2
Khales Group <it@khales.ae>
2026-01-27 15:11:57 +0400

    Auto-sync: Save latest local edits for V19

--- addons/kh_approvals/models/__init__.py
+++ addons/kh_approvals/models/__init__.py
@@ -1,1 +1,6 @@
+from . import approval_request
+from . import boq_submission
+from . import project_extension
+from . import mail_activity_guard
+from . import project_email
 from . import boq_website
