5b39eb475d39b5f13c27b8a60a37b7151063260f
Khales Group <it@khales.ae>
2026-01-27 14:24:35 +0400

    Auto-sync: Save latest local edits for V19

--- addons/kh_approvals/models/__init__.py
+++ addons/kh_approvals/models/__init__.py
@@ -1,14 +1,5 @@
 from . import approval_request
-from . import crm_extension
-from . import department
-from . import hr_employee_extension
-from . import mail_activity
-from . import mail_activity_guard
-from . import payslip_extension
-from . import project_email
-from . import project_extension
-from . import purchase_order_extension
-from . import rule_step
-from . import res_users  # <--- MAKE SURE THIS LINE EXISTS
-from . import fix_project_task
 from . import boq_submission
+from . import project_extension
+from . import res_users
+from . import hr_smart_audit
