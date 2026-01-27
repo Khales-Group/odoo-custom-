3280d4299f2f1c02a703331c656e0a4c22051aa4
Khales Group <it@khales.ae>
2026-01-27 16:40:29 +0400

    Auto-sync: Save latest local edits for V19

--- addons/kh_approvals/__manifest__.py
+++ addons/kh_approvals/__manifest__.py
@@ -10,7 +10,6 @@
     'data': [
         'security/kh_approvals_security.xml',
         'security/ir.model.access.csv',
-        'security/boq_access.csv',
         'security/kh_approvals_rules.xml',
         'security/wizard_access.xml',
         'views/menu.xml',
