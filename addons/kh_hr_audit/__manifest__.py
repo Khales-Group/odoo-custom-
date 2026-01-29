a30e25a5ed6e5e205f6b5326509cf153c01c5013
Khales Group <it@khales.ae>
2026-01-29 14:15:01 +0400

    Auto-sync: Save latest local edits for V19

--- addons/kh_hr_audit/__manifest__.py
+++ addons/kh_hr_audit/__manifest__.py
@@ -9,8 +9,8 @@
     'depends': ['base', 'hr', 'hr_attendance', 'hr_payroll', 'hr_holidays'], # تأكد أن الموديولات المعتمدة صحيحة
     'data': [
         'security/ir.model.access.csv',
-        'data/ir_cron_data.xml',
-        'data/mail_template_data.xml',
+        'ir_cron_data.xml',
+        'mail_template_data.xml',
         'views/audit_view.xml',
         'views/hr_audit_views.xml',
     ],
