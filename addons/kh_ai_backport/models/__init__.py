8d2645620c1e691ed89a0d59120eba595bd0748e
Khales Group <it@khales.ae>
2026-02-16 09:31:26 +0400

    Auto-sync: Save latest local edits for V19

--- addons/kh_ai_backport/models/__init__.py
+++ addons/kh_ai_backport/models/__init__.py
@@ -1,3 +1,5 @@
 from . import ai_agent
 from . import ai_document_chunk
 from . import ai_message
+# The ai_agent module contains both AiAgent and AiAgentSource models
+# They are automatically loaded when ai_agent is imported
