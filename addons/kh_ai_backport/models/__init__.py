a4f673060822c9ab5fc6e6b931d1b563eb72e478
Khales Group <it@khales.ae>
2026-04-12 09:56:41 +0400

    Auto-sync: Save latest local edits for V19

--- addons/kh_ai_backport/models/__init__.py
+++ addons/kh_ai_backport/models/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 from . import ai_agent_source
 from . import ai_document_chunk
-from . import ai_agent
-from . import gemini_chat
+# from . import ai_agent
+# from . import gemini_chat
