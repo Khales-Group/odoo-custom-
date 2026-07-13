bb0be54a6578ee93063d6b9b0a555cd014737587
Khales Group <it@khales.ae>
2026-07-13 11:28:12 +0400

    Auto-sync: Save latest local edits for V19

--- addons/kh_approvals/models/ir_attachment.py
+++ addons/kh_approvals/models/ir_attachment.py
@@ -1,24 +1,39 @@
 # -*- coding: utf-8 -*-
-from odoo import models
+from odoo import models, fields, api
 from odoo.tools.misc import format_datetime
 
 
 class IrAttachment(models.Model):
     _inherit = 'ir.attachment'
 
-    def _to_store(self, store, fields=None, **kwargs):
-        super()._to_store(store, fields, **kwargs)
-        for attachment in self:
-            att = attachment.sudo()
-            upload_date = ""
-            if att.create_date:
+    upload_user_name = fields.Char(
+        compute='_compute_upload_info',
+        string='Uploaded By',
+    )
+    upload_date = fields.Char(
+        compute='_compute_upload_info',
+        string='Upload Date',
+    )
+
+    @api.depends('create_uid', 'create_date')
+    def _compute_upload_info(self):
+        for att in self:
+            rec = att.sudo()
+            att.upload_user_name = rec.create_uid.name or ''
+            if rec.create_date:
                 try:
-                    upload_date = format_datetime(
-                        self.env, att.create_date, dt_format="dd MMM yyyy, HH:mm"
+                    att.upload_date = format_datetime(
+                        self.env, rec.create_date, dt_format="dd MMM yyyy, HH:mm"
                     )
                 except Exception:
-                    upload_date = att.create_date.strftime("%d %b %Y, %H:%M")
-            store.add(attachment, {
-                "upload_user_name": att.create_uid.name or "",
-                "upload_date": upload_date,
-            })
+                    att.upload_date = rec.create_date.strftime("%d %b %Y, %H:%M")
+            else:
+                att.upload_date = ''
+
+    def _to_store(self, store, fields=None, **kwargs):
+        if isinstance(fields, list):
+            fields = list(fields)
+            for f in ('upload_user_name', 'upload_date'):
+                if f not in fields:
+                    fields.append(f)
+        super()._to_store(store, fields, **kwargs)
